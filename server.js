162957407f4b0c1e311b00d27ca4e206ccb38e6d
javierfonsi <javier_fonsi@hotmail.com>
2022-11-14 19:03:24 -0500

    PORT from server

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const { sequelize } = require('./util/database')
 const { app } = require('./app');
 const { initModels } = require('./util/initModels');
 
-//const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 4000;
-}
+//let port = process.env.PORT;
+//if (port == null || port == "") {
+//  port = 4000;
+//}
 
 // Models relations
 initModels();
@@ -22,9 +22,14 @@ sequelize
     .then(() => console.log("Database syncronized"))
     .catch(error => console.log(error))
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Express app running: ${port}`);
 });
+
+
+//app.listen(port, () => {
+//    console.log(`Express app running: ${port}`);
+//});
 
 //app.listen(4000, () =>{
 //    console.log('app running')
